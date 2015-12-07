964d75fa1c3cecbae73fdcd41ed328091fbbd0d4
kschluns <kschluns@bu.edu>
2015-12-06 22:36:51 -0500

    made first changes to mash UI and Server files

--- Shiny_Control_Center/Mash/MashServer.R
+++ Shiny_Control_Center/Mash/MashServer.R
@@ -1,5 +1,19 @@
 # Mash Server
 
 mashServer <- function(input, output){
-
+        output$mashType <- renderUI({
+                if (input$MashType1 == "Step Mash (using direct heat)") {
+                        numericInput(inputId="MashNum2",label="MashNum",value=0.0)
+                } else {
+                        numericInput(inputId="MashNum1",label="MashNum",value=0.0)
+                }
+        })
+        
+        output$mashNum <- renderText({
+                if (input$MashType1 == "Step Mash (using direct heat)") {
+                        finNum = 2.0*input$MashNum2
+                } else {
+                        finNum = 5.0*input$MashNum1
+                }
+        })
 }
--- Shiny_Control_Center/Mash/MashUI.R
+++ Shiny_Control_Center/Mash/MashUI.R
@@ -2,7 +2,18 @@
 
 mashUI <- function(){
 tabPanel("Mash", 
-            #Plot Temp Mash Schedule
-            plotOutput("MashSchedule")
+         fluidRow(
+                 column(width = 4,selectInput(inputId = "MashType1",
+                                              label = "MashType",
+                                              choices =  c("Step Mash (using direct heat)",
+                                                           "Step Mash (using infusions)",
+                                                           "Single Decoction"))
+                 )
+         ),
+         fluidRow(
+                 column(width=2,uiOutput("mashType")),
+                 column(width=2,strong("You win!!"),textOutput("mashNum"))
+         ),
+         HTML("<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>")
 )
 }
