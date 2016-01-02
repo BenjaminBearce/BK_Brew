308649502b24d6cd9af915e709627f1162636b30
Benjamin Bearce <bbearce@air-worldwide.com>
2016-01-02 14:22:27 -0500

    massive changes

--- App/Water/WaterServer.R
+++ App/Water/WaterServer.R
@@ -20,20 +20,45 @@ waterServer <- function(input, output){
         output$waterGraph <- renderPlot({
                 #Grains
                 GL <- totalGrain()*input$grainAbsorptionFactor
+                #Shrinkage Reduction for equipment losses
+                shrinkage <- (1-input$shrink/100)
                 #Equipment Losses broken down
-                lauterLoss <- input$lauterTunDeadSpace
-                mashLoss <- input$mashTunDeadSpace
-                kettleLoss <- input$kettleDeadSpace
+                lauterLoss <- input$lauterTunDeadSpace*shrinkage
+                mashLoss <- input$mashTunDeadSpace*shrinkage
+                kettleLoss <- input$kettleDeadSpace*shrinkage
                 fermentationLoss <- input$fermentationTankLoss
-                
-                EL <- input$kettleDeadSpace + input$lauterTunDeadSpace + input$mashTunDeadSpace + input$fermentationTankLoss
+                print("GL")
+                print(GL)
+                print("EL")
+                EL <- lauterLoss + mashLoss + kettleLoss + fermentationLoss
+                print(EL)
+                print("BS")
                 BS <- input$batchSize
+                print(BS)
+                print("EvL")
                 EvL <- BS*((input$boilTime/60)*(input$evap/100)/(1-(input$boilTime/60)*(input$evap/100)))
+                print(EvL)
+                print("TWN")
                 TWN <- GL + EL + BS + EvL
-                print("suck my BALLLLLSSSSSSSSSSss");print(GL);print(EvL);print(EL);print(TWN)
-                x <- seq(1,180)
-                y <- rep(10,180)
-                plot(x,y)
+                print(TWN)
+                
+#                 waterData <- data.frame(step = 1:6,
+#                                         TWN = ,
+#                                         PostMash = ,
+#                                         PostSparge = ,
+#                                         PostBoil = ,
+#                                         Cooling = ,
+#                                         Fermentation = )
+                
+                
+                waterData <- data.frame(step = seq(1,6), vol = c(TWN,
+                                                                 TWN-GL,
+                                                                 TWN-GL-mashLoss,
+                                                                 TWN-GL-mashLoss-lauterLoss,
+                                                                 TWN-GL-mashLoss-lauterLoss-kettleLoss-EvL,
+                                                                 TWN-GL-mashLoss-lauterLoss-kettleLoss-EvL-fermentationLoss))
+                waterGraph <- ggplot(data = waterData, aes(step,vol))
+                waterGraph + ggtitle("Total Water Needed") + geom_step()
         })
         
 }
