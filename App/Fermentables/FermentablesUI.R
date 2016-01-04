2a17851b03d88f17c2d1272474fd9bfefda6bab1
kschluns <kschluns@bu.edu>
2016-01-04 01:15:49 -0500

    - fixed bug where non-grain fermentables were throwing errors by adding new variable Grists in Control_Center and FermentablesServer, whereas the old method was only using the Grains variable - cleaned up FermentableServers.R by making functions for grain calculations to be called for each ingredient instead of replicating code for each ingredient - added feature for diastatic power (DP) calculation, which alerts user if DP is too low ONLY WHEN (1 - the user has completed filling out its ingredients with percents that add up to 100%) AND (2 - user elects to check DP) AND (3- the user has at least one grain)

--- App/Fermentables/FermentablesUI.R
+++ App/Fermentables/FermentablesUI.R
@@ -1,87 +1,102 @@
 # Fermentables UI
 
 fermentablesUI <- function(){
-tabPanel("Fermentables",
-         fluidRow(
-                 column(width = 2,
-                        selectInput(inputId = "Ingredients1",
-                                    label = "Ingredients",
-                                    choices =  c(Grains$Ingredients, Extracts$Ingredients, Adjuncts$Ingredients))
+        tabPanel("Fermentables",
+                 HTML("<br>"),
+                 fluidRow(
+                         column(width = 4,
+                                selectInput(inputId = "DPcheck",
+                                            label = "Fermentables Difficulty",
+                                            choices = c("Default","Advanced")),
+                                bsTooltip("DPcheck","Advanced will alert user when Diastatic Power is too low for self conversion","top","hover focus")
+                         ),
+                         column(width = 4,
+                                uiOutput("DPcalc")
+                         ),
+                         column(width = 4,
+                                uiOutput("DPalert"))
                  ),
-                 column(width = 2,
-                        numericInput(inputId = "IngredientPercent1",
-                                     label = "Percent",
-                                     value = 0)
+                 fluidRow(
+                         column(width = 4,
+                                selectInput(inputId = "Ingredients1",
+                                            label = "Ingredients",
+                                            choices =  c(Grains$Ingredients, Extracts$Ingredients, Adjuncts$Ingredients))
+                         ),
+                         column(width = 2,
+                                numericInput(inputId = "IngredientPercent1",
+                                             label = "Percent",
+                                             value = 0)
+                         ),
+                         column(width = 2,
+                                strong("Lbs"),
+                                verbatimTextOutput(outputId = "OG1")
+                         ),
+                         column(width = 2,
+                                strong("Total Grains (Lbs)"),
+                                verbatimTextOutput(outputId = "fermentablesTotalGrain")
+                         )
+                         
                  ),
-                 column(width = 2,
-                        strong("Lbs"),
-                        textOutput(outputId = "OG1")
+                 fluidRow(
+                         column(width = 4,
+                                selectInput(inputId = "Ingredients2",
+                                            label = NULL,
+                                            choices =  c(Grains$Ingredients, Extracts$Ingredients, Adjuncts$Ingredients))
+                         ),
+                         column(width = 2,
+                                numericInput(inputId = "IngredientPercent2",
+                                             label = NULL,
+                                             value = 0)
+                         ),
+                         column(width = 2,
+                                verbatimTextOutput(outputId = "OG2")
+                         )
                  ),
-                 column(width = 2,
-                        strong("Total Grains (Lbs)"),
-                        textOutput(outputId = "fermentablesTotalGrain")
-                 )
-                 
-         ),
-         fluidRow(
-                 column(width = 2,
-                        selectInput(inputId = "Ingredients2",
-                                    label = NULL,
-                                    choices =  c(Grains$Ingredients, Extracts$Ingredients, Adjuncts$Ingredients))
+                 fluidRow(
+                         column(width = 4,
+                                selectInput(inputId = "Ingredients3",
+                                            label = NULL,
+                                            choices =  c(Grains$Ingredients, Extracts$Ingredients, Adjuncts$Ingredients))
+                         ),
+                         column(width = 2,
+                                numericInput(inputId = "IngredientPercent3",
+                                             label = NULL,
+                                             value = 0)
+                         ),
+                         column(width = 2,
+                                verbatimTextOutput(outputId = "OG3")
+                         )
                  ),
-                 column(width = 2,
-                        numericInput(inputId = "IngredientPercent2",
-                                     label = NULL,
-                                     value = 0)
+                 fluidRow(
+                         column(width = 4,
+                                selectInput(inputId = "Ingredients4",
+                                            label = NULL,
+                                            choices =  c(Grains$Ingredients, Extracts$Ingredients, Adjuncts$Ingredients))
+                         ),
+                         column(width = 2,
+                                numericInput(inputId = "IngredientPercent4",
+                                             label = NULL,
+                                             value = 0)
+                         ),
+                         column(width = 2,
+                                verbatimTextOutput(outputId = "OG4")
+                         )
                  ),
-                 column(width = 2,
-                        textOutput(outputId = "OG2")
-                 )
-         ),
-         fluidRow(
-                 column(width = 2,
-                        selectInput(inputId = "Ingredients3",
-                                    label = NULL,
-                                    choices =  c(Grains$Ingredients, Extracts$Ingredients, Adjuncts$Ingredients))
+                 fluidRow(
+                         column(width = 4,
+                                selectInput(inputId = "Ingredients5",
+                                            label = NULL,
+                                            choices =  c(Grains$Ingredients, Extracts$Ingredients, Adjuncts$Ingredients))
+                         ),
+                         column(width = 2,
+                                numericInput(inputId = "IngredientPercent5",
+                                             label = NULL,
+                                             value = 0)
+                         ),
+                         column(width = 2,
+                                verbatimTextOutput(outputId = "OG5")
+                         )
                  ),
-                 column(width = 2,
-                        numericInput(inputId = "IngredientPercent3",
-                                     label = NULL,
-                                     value = 0)
-                 ),
-                 column(width = 2,
-                        textOutput(outputId = "OG3")
-                 )
-         ),
-         fluidRow(
-                 column(width = 2,
-                        selectInput(inputId = "Ingredients4",
-                                    label = NULL,
-                                    choices =  c(Grains$Ingredients, Extracts$Ingredients, Adjuncts$Ingredients))
-                 ),
-                 column(width = 2,
-                        numericInput(inputId = "IngredientPercent4",
-                                     label = NULL,
-                                     value = 0)
-                 ),
-                 column(width = 2,
-                        textOutput(outputId = "OG4")
-                 )
-         ),
-         fluidRow(
-                 column(width = 2,
-                        selectInput(inputId = "Ingredients5",
-                                    label = NULL,
-                                    choices =  c(Grains$Ingredients, Extracts$Ingredients, Adjuncts$Ingredients))
-                 ),
-                 column(width = 2,
-                        numericInput(inputId = "IngredientPercent5",
-                                     label = NULL,
-                                     value = 0)
-                 ),
-                 column(width = 2,
-                        textOutput(outputId = "OG5")
-                 )
-         )
-)
+                 HTML("<br><br><br><br><br>")
+        )
 }
