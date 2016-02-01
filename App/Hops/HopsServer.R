4c22436066e84543946ddada7b77eb5f0d5480c9
Benjamin Bearce <bbearce@air-worldwide.com>
2016-02-01 00:23:50 -0500

    Hops formatting

--- App/Hops/HopsServer.R
+++ App/Hops/HopsServer.R
@@ -1,7 +1,37 @@
 # Hops Server
 
 hopsServer <- function(input, output){
-        output$hopsTotalGrain <- renderText({
-               "Q"# totalGrain()
+        output$hopsUtilization1 <- renderText({
+                "Utilization1"
+        })
+        output$hopsUtilization2 <- renderText({
+                "Utilization2"
+        })
+        output$hopsUtilization3 <- renderText({
+                "Utilization3"
+        })
+        output$hopsUtilization4 <- renderText({
+                "Utilization4"
+        })
+        output$hopsUtilization5 <- renderText({
+                "Utilization5"
+                
+        })
+        
+        output$hopsIBU1 <- renderText({
+                "IBU1"
+        })
+        output$hopsIBU2 <- renderText({
+                "IBU2"
+        })
+        output$hopsIBU3 <- renderText({
+                "IBU3"
+        })
+        output$hopsIBU4 <- renderText({
+                "IBU4"
+        })
+        output$hopsIBU5 <- renderText({
+                "IBU5"
+                
         })
 }
--- App/Hops/HopsUI.R
+++ App/Hops/HopsUI.R
@@ -26,13 +26,126 @@ tabPanel("Hops",
                 ),
                 column(width = 2,
                        strong("Utilization (%)"),
-                       wellPanel("To Be Calculated...") #switch to verbatimTextOutput() for reactive values
+                       verbatimTextOutput(outputId = "hopsUtilization1") #switch to verbatimTextOutput() for reactive values
                 ),
                 column(width = 2,
                        strong("IBU"),
-                       wellPanel("To Be Calculated...") #switch to verbatimTextOutput() for reactive values
+                       verbatimTextOutput(outputId = "hopsIBU1") #switch to verbatimTextOutput() for reactive values
                 )
-        )
+        ),
+        fluidRow(
+                column(width = 2,
+                       selectInput(inputId = "Hops2",
+                                   label = NULL,
+                                   choices =  c(1,2,3))
+                ),
+                column(width = 2,
+                       numericInput(inputId = "Weight2",
+                                    label = NULL,
+                                    value = 0)
+                ),
+                column(width = 2,
+                       numericInput(inputId = "AAU2",
+                                    label = NULL,
+                                    value = 0)
+                ),
+                column(width = 2,
+                       numericInput(inputId = "BoilTime2",
+                                    label = NULL,
+                                    value = 0)
+                ),
+                column(width = 2,
+                       verbatimTextOutput(outputId = "hopsUtilization2") #switch to verbatimTextOutput() for reactive values
+                ),
+                column(width = 2,
+                       verbatimTextOutput(outputId = "hopsIBU2") #switch to verbatimTextOutput() for reactive values
+                )
+        ),
+        fluidRow(
+                column(width = 2,
+                       selectInput(inputId = "Hops3",
+                                   label = NULL,
+                                   choices =  c(1,2,3))
+                ),
+                column(width = 2,
+                       numericInput(inputId = "Weight3",
+                                    label = NULL,
+                                    value = 0)
+                ),
+                column(width = 2,
+                       numericInput(inputId = "AAU3",
+                                    label = NULL,
+                                    value = 0)
+                ),
+                column(width = 2,
+                       numericInput(inputId = "BoilTime3",
+                                    label = NULL,
+                                    value = 0)
+                ),
+                column(width = 2,
+                       verbatimTextOutput(outputId = "hopsUtilization3") #switch to verbatimTextOutput() for reactive values
+                ),
+                column(width = 2,
+                       verbatimTextOutput(outputId = "hopsIBU3") #switch to verbatimTextOutput() for reactive values
+                )
+        ),
+        fluidRow(
+                column(width = 2,
+                       selectInput(inputId = "Hops4",
+                                   label = NULL,
+                                   choices =  c(1,2,3))
+                ),
+                column(width = 2,
+                       numericInput(inputId = "Weight4",
+                                    label = NULL,
+                                    value = 0)
+                ),
+                column(width = 2,
+                       numericInput(inputId = "AAU4",
+                                    label = NULL,
+                                    value = 0)
+                ),
+                column(width = 2,
+                       numericInput(inputId = "BoilTime4",
+                                    label = NULL,
+                                    value = 0)
+                ),
+                column(width = 2,
+                       verbatimTextOutput(outputId = "hopsUtilization4") #switch to verbatimTextOutput() for reactive values
+                ),
+                column(width = 2,
+                       verbatimTextOutput(outputId = "hopsIBU4") #switch to verbatimTextOutput() for reactive values
+                )
+        ),
+        fluidRow(
+                column(width = 2,
+                       selectInput(inputId = "Hops5",
+                                   label = NULL,
+                                   choices =  c(1,2,3))
+                ),
+                column(width = 2,
+                       numericInput(inputId = "Weight5",
+                                    label = NULL,
+                                    value = 0)
+                ),
+                column(width = 2,
+                       numericInput(inputId = "AAU5",
+                                    label = NULL,
+                                    value = 0)
+                ),
+                column(width = 2,
+                       numericInput(inputId = "BoilTime5",
+                                    label = NULL,
+                                    value = 0)
+                ),
+                column(width = 2,
+                       verbatimTextOutput(outputId = "hopsUtilization5") #switch to verbatimTextOutput() for reactive values
+                ),
+                column(width = 2,
+                       verbatimTextOutput(outputId = "hopsIBU5") #switch to verbatimTextOutput() for reactive values
+                )
+        ),
+        HTML("<br><br><br><br><br>")
 
 )
 }
