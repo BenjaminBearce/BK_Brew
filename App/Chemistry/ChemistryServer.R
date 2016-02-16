8ad5029ce2d37a83f7da4ff13044ba57b15ef1c5
Benjamin Bearce <bbearce@air-worldwide.com>
2016-02-15 21:28:30 -0500

    Chemistry stuff 1

--- App/Chemistry/ChemistryServer.R
+++ App/Chemistry/ChemistryServer.R
@@ -1,5 +1,67 @@
 # Chemistry Server
 
 chemistryServer <- function(input, output){
-
+        output$pH <- renderText({
+                estimatedPH <- 5.8 + (input$HCO3_CaCO3*0.056 - input$Ca*0.04 - input$Mg*0.033)*0.028
+        })
+        
+        #we have inputs for 
+        #Ca - using
+        #Mg - using
+        #NA - not using
+        #Cl - not using
+        #SO4 - not using
+        #HCO3_CaCO3 - using
+        
+        output$grainInfo <- renderTable({
+                #Grain Type and Distilled Water pH
+                grainDistpH <- data.frame(grainTypes = 1:11,
+                                          distWaterpH = c(0,
+                                                          5.7,
+                                                          5.79,
+                                                          5.77,
+                                                          5.43,
+                                                          5.75,
+                                                          6.04,
+                                                          5.56,
+                                                          5.70,
+                                                          NA,
+                                                          4.71))
+                
+                #Grains
+                ingredients <- c(input$Ingredients1,
+                                 input$Ingredients2,
+                                 input$Ingredients3,
+                                 input$Ingredients4,
+                                 input$Ingredients5)
+                
+                #Lbs
+                lbs <- as.numeric(c(grains1(),
+                                    grains2(),
+                                    grains3(),
+                                    grains4(),
+                                    grains5()))
+                #EZ Water Code
+                ezWaterCode <- as.numeric(c(filter(Grists, Ingredients == input$Ingredients1) %>% select(EZWaterCode),
+                                          filter(Grists, Ingredients == input$Ingredients2) %>% select(EZWaterCode),
+                                          filter(Grists, Ingredients == input$Ingredients3) %>% select(EZWaterCode),
+                                          filter(Grists, Ingredients == input$Ingredients4) %>% select(EZWaterCode),
+                                          filter(Grists, Ingredients == input$Ingredients5) %>% select(EZWaterCode)))
+                #SRM
+                srm <- as.numeric(c(filter(Grists, Ingredients == input$Ingredients1) %>% select(SRM),
+                                    filter(Grists, Ingredients == input$Ingredients2) %>% select(SRM),
+                                    filter(Grists, Ingredients == input$Ingredients3) %>% select(SRM),
+                                    filter(Grists, Ingredients == input$Ingredients4) %>% select(SRM),
+                                    filter(Grists, Ingredients == input$Ingredients5) %>% select(SRM)))
+                
+                #Distilled Water Mash pH
+                distWaterMashpH <- 
+                
+                data.frame(Ingredients =ingredients, 
+                           Lbs = lbs, 
+                           SRM = srm,
+                           EZWaterCode = ezWaterCode) %>%
+                        left_join(y = grainDistpH, by = c("EZWaterCode" = "grainTypes")) %>%
+                        mutate(distWaterpH = ifelse(ezWaterCode == 10, 5.22-0.00504*srm,distWaterpH))
+        })
 }
--- App/Chemistry/ChemistryUI.R
+++ App/Chemistry/ChemistryUI.R
@@ -1,5 +1,59 @@
 # Chemistry UI
 
 chemistryUI <- function(){
-        tabPanel("Chemistry", "Chemistry")
+        tabPanel("Chemistry",
+                fluidRow(
+                        h4("Step1: Initial Ion Concentrations (ppm or mg/L)"),
+                        column(width = 1,
+                               numericInput(inputId = "Ca",
+                                            label = "Ca",
+                                            value =  4)
+                        ),
+                        column(width = 1,
+                               numericInput(inputId = "Mg",
+                                            label = "Mg",
+                                            value =  1)
+                        ),
+                        column(width = 1,
+                               numericInput(inputId = "Na",
+                                            label = "Na",
+                                            value =  32)
+                        ),
+                        column(width = 1,
+                               numericInput(inputId = "Cl",
+                                            label = "Cl",
+                                            value =  27)
+                        ),
+                        column(width = 1,
+                               numericInput(inputId = "SO4",
+                                            label = "CaCO3",
+                                            value =  6)
+                        ),
+                        column(width = 2,
+                               numericInput(inputId = "HCO3_CaCO3",
+                                            label = "HCO3 or CaCO3",
+                                            value =  40)
+                        )
+                ),
+                fluidRow(
+                        h4("Step2: Grain Info"),
+                        tableOutput(outputId = "grainInfo")
+                ),
+                fluidRow(
+                        h4("Step3: View Mash pH"),
+                        column(width = 2,
+                               strong("Estimated Room Temp pH"),
+                               verbatimTextOutput(outputId = "pH")
+                        )
+                ),
+                fluidRow(
+                        h4("Step4(a): Adjust Mash pH Down (if needed)")
+                ),
+                fluidRow(
+                        h4("Step4(b): Adjust Mash pH Up (if needed)")
+                ),
+                fluidRow(
+                        h4("Step5: View Resulting Water Profile")
+                )
+        )
 }
